98d3a86ccc490370d7edf6f55970399ef861ecf5
Tadas Zukauskas <Tadas Zukauskas>
2017-11-14 19:36:53 +0200

    Add project files.

---
+++ 1-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vardas = "Tadas";
+            var Pavarde = "Zukauskas";
+            var amzius = "32";
+            var ugis = "1.86";
+            var svoris = "88";
+            var aukstoji mokykla = "kaunascodingschool";
+            var akademines grupes kodas = "1";
+            var Studiju programos pavadinimas = "programavimas";
+            var atsiskaitytu kreditu skaicius = "3";
+                     }
+    }
+}
